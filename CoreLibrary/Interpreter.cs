12a1c92626e72f0a9727c23054862001e2856964
Ruan Montelo <ruanmontelo@gmail.com>
2023-10-17 20:17:29 -0300

    Initial commit

---
+++ CoreLibrary/Interpreter.cs
@@ -0,0 +1,78 @@
+﻿namespace CoreLibrary;
+
+/// <summary>
+/// The interpreter class. For now, it's all static.
+/// </summary>
+public class Interpreter
+{
+    private readonly char[] _validCommands = { '-', '+', '<', '>', '[', ']', ',', '.' };
+
+    private byte[] _pointerArray = new byte[30000];
+
+    private int _pointer;
+
+    public Interpreter()
+    {
+        for (var i = 0; i < _pointerArray.Length; i++)
+        {
+            _pointerArray[i] = 0;
+        }
+    }
+    
+    public void Execute(string input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            char ch = input[i];
+            
+            if (!_validCommands.Contains(ch))
+            {
+                continue;
+            }
+
+            switch (ch)
+            {
+                case '>':
+                    _pointer++;
+                    break;
+                case '<':
+                    _pointer--;
+                    break;
+                case '+':
+                    _pointerArray[_pointer]++;
+                    break;
+                case '-':
+                    _pointerArray[_pointer]--;
+                    break;
+                case '.':
+                    Console.Write((char)_pointerArray[_pointer]);
+                    break;
+                case '[':
+                {
+                    if (_pointerArray[_pointer] == 0)
+                    {
+                        do
+                        {
+                            i++;
+                        } while (input[i] != ']' && i < input.Length);
+
+                        i++;
+                    }
+                    break;
+                }
+
+                case ']':
+                    if (_pointerArray[_pointer] != 0)
+                    {
+                        do
+                        {
+                            i--;
+                        } while (input[i] != '[' && i > 0);
+                    }
+                    break;
+            }
+        }
+        
+        Console.WriteLine();
+    }
+}
---
+++ Executor/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CoreLibrary;
+
+Console.WriteLine("Welcome to the Brainf**k Interpreter...");
+
+Console.WriteLine("Please note that: ");
+Console.WriteLine("Any invalid command will be ignored.");
+Console.WriteLine("Memory usage is limited to 30000 bytes.");
+
+while (true)
+{
+    Console.Write($"{DateTime.Now}: ");
+
+    string? input = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(input))
+    {
+        continue;
+    }
+
+    Interpreter interpreter = new();
+
+    interpreter.Execute(input);
+}
